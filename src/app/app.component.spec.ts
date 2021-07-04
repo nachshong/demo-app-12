6d8c643345ae1c177a26ddcc07db94b2ff0b86dc
Nachshon Givoni <28922640+nachshong@users.noreply.github.com>
2021-07-04 15:17:28 +0300

    New @angular/cli@12.1.1 project with @angular/material.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('demo-app-v12a app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('demo-app app is running!');
   });
 });
