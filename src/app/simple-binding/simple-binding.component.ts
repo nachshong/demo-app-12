dcb7fe54bb18c41bfdbfaf0e7a4352c5c0ef0e13
Nachshon Givoni <28922640+nachshong@users.noreply.github.com>
2021-07-05 13:45:15 +0300

    Lesson 3: simple binding

--- src/app/simple-binding/simple-binding.component.ts
+++ src/app/simple-binding/simple-binding.component.ts
@@ -7,9 +7,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SimpleBindingComponent implements OnInit {
 
+  kingName: string | undefined;
+  firstName: string = '';
+  middleName: string = '';
+  lastName: string = '';
+  boxSize: number = 0;
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.boxSize = 100;
+  }
+
+  changeToDavid() {
+    this.kingName = "דוד המלך"
+  }
+
+  changeToSolomon() {
+    this.kingName = "שלמה המלך"
+  }
+
+  setFirstNameByEvent(event: Event) {
+    this.firstName = (<HTMLInputElement>event.target).value;
+  }
+
+  setMiddleNameByInput(input: HTMLInputElement) {
+    this.middleName = input.value;
+  }
+
+  setMiddleNameByValue(value: string) {
+    this.middleName = value;
+  }
+
+  incBoxSize() {
+    if (this.boxSize < 200) {
+      this.boxSize += 10;
+    }
+
+    return false;
+  }
+
+  decBoxSize() {
+    if (this.boxSize > 100) {
+      this.boxSize -= 10;
+    }
+
+    return false;
   }
 
 }
